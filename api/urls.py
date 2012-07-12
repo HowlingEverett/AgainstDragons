a24b54cf4330423de644a2063f7b565b974b2f8d
Justin Marrington <justin@marrington.net>
2012-07-12 12:56:02 +1000

    Added view and url for registering users via the API

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     
     url(r'^$', ApiIndexView.as_view(), name="api_docs"),
     url(r'^login/$', APILoginView.as_view(), name='api_login'),
+    url(r'^register/$', APIRegisterView.as_view(), name='api_register'),
     url(r'^batch_upload/$', BatchSampleUploadView.as_view(), name='batch_upload'),
     url(r'^surveys/$', SurveyListView.as_view(), name='surveys')
 )
--- api/views.py
+++ api/views.py
@@ -16,28 +16,31 @@ from django.contrib.gis.geos import Point
 from geosurvey.models import *
 
 
-
 class BaseAPIResponseMixin(object):
     pass
-    
+
+
 class JSONAPIResponseMixin(BaseAPIResponseMixin):
-    
     def error_response(self, request_errors, field_errors=None):
         """ Returns a JSON-formatted error response with a 402 Bad Request
             response code. 
             
-            error_response(['error description', ...], {'field_name':'error description'})
+            error_response(['error description', ...],
+            {'field_name':'error description'})
                 -> "{'request_errors': ['error1', 'error2', ...], 
-                     'field_errors': {'field1':'error desc', 'field2': 'error_desc}}"
+                     'field_errors': {'field1':'error desc',
+                     'field2': 'error_desc}}"
         """
-        error_dict = {'request_errors':None}
-        return HttpResponseBadRequest(json.dumps(error_dict), content_type="text/json")
-        
+        error_dict = {'request_errors': None}
+        return HttpResponseBadRequest(json.dumps(error_dict),
+                                      content_type="text/json")
+
     def success_response(self, success_dict):
         """ Returns a JSON-formattted success response with a 200 response code.
             
         """
         return HttpResponse(json.dumps(success_dict), content_type="text/json")
+
 
 class APILoginView(JSONAPIResponseMixin, View):
     @method_decorator(csrf_exempt)
@@ -50,14 +53,38 @@ class APILoginView(JSONAPIResponseMixin, View):
             return self.success_response({'success': 'User logged in.'})
         else:
             return HttpResponse
-            return self.error_response({'error': 'Incorrect username or password.'})
+            return self.error_response(
+                    {'error': 'Incorrect username or password.'})
+
+
+class APIRegisterView(JSONAPIResponseMixin, View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        existing = User.objects.filter(username__iexact=username)
+        if len(existing) > 0:
+            return self.error_response(
+                    {'error': 'User with that username already exists.'})
+        if len(password) == 0 or len(email) == 0:
+            return self.error_response({
+                'error': 'You must enter a valid password and email address.'})
+
+        User.objects.create_user(username, email, password)
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+
+        return self.success_response(
+                {'success': 'User created and logged in successfully'})
 
 
 class BatchSampleUploadView(JSONAPIResponseMixin, View):
     """ Performs a batch create of GeographicalSamples for a single trip
     
     """
-    
+
     @method_decorator(login_required)
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
@@ -87,23 +114,27 @@ class BatchSampleUploadView(JSONAPIResponseMixin, View):
         trips = payload.get('trips')
         if not trips:
             return self.error_response(['No trips included in request body.'])
-        
+
         samples = []
         for trip_data in trips:
             trip_dict = trip_data.get('trip')
             samples_list = trip_data.get('samples')
             if not trip_dict:
-                return self.error_response(['No trip description included in your trips.'])
+                return self.error_response(
+                    ['No trip description included in your trips.'])
             if not samples_list:
-                return self.error_response(['No samples included for trip {0}.'.format(trip_dict['date'])])
-            
+                return self.error_response([
+                    'No samples included for trip {0}.'.format(
+                        trip_dict['date'])])
+
             trip = self._create_trip(trip_dict, request)
             samples += self._create_samples(samples_list, trip, request)
-        
+
         GeographicalSample.objects.bulk_create(samples)
-        
-        return self.success_response({'success': 'Trips successfully uploaded to survey.'})
-        
+
+        return self.success_response(
+                {'success': 'Trips successfully uploaded to survey.'})
+
     def _create_trip(self, trip_dict, request):
         trip = Trip(
             date=datetime.strptime(trip_dict['date'], '%Y-%m-%d').date(),
@@ -118,13 +149,15 @@ class BatchSampleUploadView(JSONAPIResponseMixin, View):
                 mode=mode
             )
         return trip
-    
+
     def _create_samples(self, samples_list, trip, request):
         samples = []
         for sample in samples_list:
             geosample = GeographicalSample(
-                location=Point(float(sample['longitude']), float(sample['latitude']), srid=4326),
-                timestamp=datetime.strptime(sample['timestamp'], '%Y-%m-%d %H:%M:%S'),
+                location=Point(float(sample['longitude']),
+                               float(sample['latitude']), srid=4326),
+                timestamp=datetime.strptime(sample['timestamp'],
+                                            '%Y-%m-%d %H:%M:%S'),
                 speed=sample.get('speed', None),
                 heading=sample.get('heading', None),
                 location_accuracy=sample.get('location_accuracy', None),
@@ -150,14 +183,17 @@ class BatchSampleUploadView(JSONAPIResponseMixin, View):
 
         return samples
 
-class ApiIndexView(TemplateView):
-    template_name='api/index.html'  
-    
-class SurveyListView(View):
 
+class ApiIndexView(TemplateView):
+    template_name = 'api/index.html'
+
+
+class SurveyListView(View):
     @method_decorator(require_GET)
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
-        surveys = Survey.objects.filter(end_date__gte=datetime.today()).order_by('end_date')
-        return HttpResponse(serializers.serialize('json', surveys), content_type='text/json')
+        surveys = Survey.objects.filter(
+            end_date__gte=datetime.today()).order_by('end_date')
+        return HttpResponse(serializers.serialize('json', surveys),
+                            content_type='text/json')
 
