d06aebdf7df7b5f410a6a2edd636e935db448b25
Justin Marrington <justin@marrington.net>
2012-08-06 14:56:23 +1000

    Added manual gunicorn execution back to fabfile

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -62,7 +62,11 @@ def configure_deployment():
     with cd(env.project_root):
         sudo("chmod 755 manage.py")
         run("%s/bin/python manage.py collectstatic --noinput -v0" % code_dir)
+<<<<<<< HEAD
         sudo("pkill python; %s/bin/python manage.py run_gunicorn -D -p /tmp/gunicorn_againstdragons.pid --user=www-data --group=www-data --error-logfile=/var/log/gunicorn/gunicorn.log 127.0.0.1:9006")
+=======
+        sudo("pkill python; %s/bin/python manage.py run_gunicorn -D -p /tmp/gunicorn_againstdragons.pid --user=www-data --group=www-data --error-logfile=/var/log/gunicorn/gunicorn.log 127.0.0.1:9006" % code_dir)
+>>>>>>> Added manual gunicorn execution back to fabfile
         
 
 def deploy():
